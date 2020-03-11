dbd7d6fcbc5a919a9534ddbe26854ec8561ca0d1
Simão Reis <smnrsti@gmail.com>
2020-03-11 17:04:03 +0000

    tests: Add konvoy manager GetNodeNamesForNodeGroup tests
    Signed-off-by: Simão Reis <smnrsti@gmail.com>

--- cluster-autoscaler/cloudprovider/konvoy/konvoy_manager_test.go
+++ cluster-autoscaler/cloudprovider/konvoy/konvoy_manager_test.go
@@ -1,11 +1,15 @@
 package konvoy
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	kubernetesfake "k8s.io/client-go/kubernetes/fake"
+	kubernetestesting "k8s.io/client-go/testing"
 	utilpointer "k8s.io/utils/pointer"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 
@@ -98,6 +102,100 @@ func TestKonvoyManagerGetNodeGroups(t *testing.T) {
 		for i := range nodeGroups {
 			nodeGroups[i].konvoyManager = nil
 		}
-		assert.Equal(t, test.nodeGroups, nodeGroups, "%s: expected %v got %v", test.description, test.nodeGroups, nodeGroups)
+		assert.Equal(t, test.nodeGroups, nodeGroups, test.description)
 	}
+}
+
+func TestKonvoyManagerGetNodeNamesForNodeGroup(t *testing.T) {
+
+	var tests = []struct {
+		description   string
+		err           error
+		expectedNodes []string
+		node          *corev1.Node
+		nodeGroup     string
+		provisioner   string
+	}{
+		{
+			description:   "should return empty node names list",
+			nodeGroup:     "test-node-group",
+			expectedNodes: []string{},
+			node:          &corev1.Node{},
+		},
+		{
+			description:   "should return list with a node name",
+			nodeGroup:     "test-node-group",
+			expectedNodes: []string{"test-node"},
+			node: &corev1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test-node",
+					Labels: map[string]string{
+						nodeGroupLabel: "test-node-group",
+					},
+				},
+			},
+		},
+		{
+			description:   "should return list with node provider id",
+			nodeGroup:     "test-node-group",
+			expectedNodes: []string{"test-provider-id"},
+			node: &corev1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test-node",
+					Labels: map[string]string{
+						nodeGroupLabel: "test-node-group",
+					},
+				},
+				Spec: corev1.NodeSpec{
+					ProviderID: "test-provider-id",
+				},
+			},
+			provisioner: "aws",
+		},
+		{
+			description:   "should return empty list because node does not have required label",
+			nodeGroup:     "test-node-group",
+			expectedNodes: []string{},
+			node: &corev1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test-node",
+					Labels: map[string]string{
+						"foo": "test-node-group",
+					},
+				},
+			},
+		},
+		{
+			description: "should return an error listing nodes",
+			err:         fmt.Errorf("fake error"),
+			node:        &corev1.Node{},
+		},
+	}
+
+	scheme := runtime.NewScheme()
+	err := konvoyclusterv1beta1.AddToScheme(scheme)
+	assert.NoError(t, err)
+
+	for _, test := range tests {
+
+		kubeClient := kubernetesfake.NewSimpleClientset(test.node)
+
+		mgr := &KonvoyManager{
+			kubeClient:  kubeClient,
+			provisioner: test.provisioner,
+		}
+
+		if test.err != nil {
+			reactor := func(action kubernetestesting.Action) (handled bool, ret runtime.Object, err error) {
+				return true, nil, test.err
+			}
+			kubeClient.PrependReactor("list", "nodes", reactor)
+		}
+
+		nodes, err := mgr.GetNodeNamesForNodeGroup(test.nodeGroup)
+
+		assert.Equal(t, test.err, err)
+		assert.Equal(t, test.expectedNodes, nodes, test.description)
+	}
+
 }
